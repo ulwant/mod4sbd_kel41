a47b09adf364d64af076004b123131bb7a8aa24b
Nama Kamu <email@example.com>
2026-04-23 21:37:13 +0700

    fix: serve static files from root folder

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import bookRoutes from './routes/bookRoutes.js';
 import loanRoutes from './routes/loanRoutes.js';
-import memberRoutes from './routes/memberRoutes.js'; 
+import memberRoutes from './routes/memberRoutes.js';
 import authorRoutes from './routes/authorRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import reportRoutes from './routes/reportRoutes.js';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..'))); // ← ini yang diubah
 
 // Grouping Routes
 app.use('/api/books', bookRoutes);
